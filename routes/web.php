2703463425eae447eac02e4e335099240cf9115e
Kaleb0001 <kalebamessinou@gmail.com>
2026-05-25 19:38:01 +0000

    CRUD Ad

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BackOffice\AdController;
 use App\Http\Controllers\BackOffice\SectionController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,13 @@ Route::prefix('admin')->group(function () {
     Route::post('/sections/{id}/delete', [SectionController::class, 'destroy']);
     Route::post('/sections/{id}/toggle', [SectionController::class, 'toggle']);
     Route::post('/sections/{id}/move', [SectionController::class, 'move']);
+
+
+    Route::get('/ads', [AdController::class, 'index']);
+    Route::get('/ads/create', [AdController::class, 'create']);
+    Route::post('/ads', [AdController::class, 'store']);
+    Route::get('/ads/{id}/edit', [AdController::class, 'edit']);
+    Route::post('/ads/{id}', [AdController::class, 'update']);
+    Route::post('/ads/{id}/delete', [AdController::class, 'destroy']);
+    Route::post('/ads/{id}/toggle', [AdController::class, 'toggle']);
 });
